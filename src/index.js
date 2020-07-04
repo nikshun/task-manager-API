4e59b0d5dd50744632151a241f91aff7bbce82ea
nmyshuniaiev <nmyshuniaiev@gmail.com>
2020-07-04 23:11:13 +0300

    Added Bootstrap template

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
+require('./db/mongoose')
+const path = require('path')
 const express = require('express')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
+const homeRouter = require('./routers/home')
+const port = process.env.PORT
 const loggingMiddleware = require('./middleware/log')
-require('./db/mongoose')
+const viewsPath = path.join(__dirname, '../templates')
 
 const app  = express()
-const port = process.env.PORT
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
 
 app.use(loggingMiddleware)
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+app.use(homeRouter)
 
 app.listen(port, () => {
     console.log("Server is up on port " + port)
